7cd3a11af5480c08b51eac116fd9623a2d6b2b21
wuhao <wuhao@nodeplus.io>
2022-06-15 16:49:44 +0800

    add test scripts

---
+++ test/test.js
@@ -0,0 +1,147 @@
+const chai = require('chai');
+const expect = chai.expect
+const { ethers } = require('hardhat');
+const BigNumber = require('bignumber.js')
+
+describe('MasterChef test', function() {
+
+    async function getMultiplier(_from, _to, BONUS_MULTIPLIER) {
+        return _to.sub(_from).mul(BONUS_MULTIPLIER);
+    }
+
+    async function pendingKiKi(pool, user, lpSupply, blockNumber, kikiPerBlock, totalAllocPoint, BONUS_MULTIPLIER) {
+        let accKiKiPerShare = pool.accKiKiPerShare;
+        if (blockNumber > pool.lastRewardBlock && lpSupply != 0) {
+            let multiplier = await getMultiplier(pool.lastRewardBlock, blockNumber, BONUS_MULTIPLIER);
+            let kikiReward = kikiPerBlock.mul(multiplier).mul(pool.allocPoint).div(totalAllocPoint);
+            accKiKiPerShare = accKiKiPerShare.add(kikiReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accKiKiPerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    async function leaveStaking(account, amount, kikiToken, syrupBar, masterChef) {
+        let oldStakingInMasterChef = await kikiToken.balanceOf(masterChef.address);
+        let oldStakingInSyrupBar = await syrupBar.balanceOf(account.address);
+        let oldBalanceInKiKi = await kikiToken.balanceOf(account.address);
+        let pool = await masterChef.poolInfo(0);
+        let user = await masterChef.userInfo(0, account.address);
+        let lpSupply = oldStakingInMasterChef;
+        let kikiPerBlock = await masterChef.kikiPerBlock();
+        let totalAllocPoint = await masterChef.totalAllocPoint();
+        let BONUS_MULTIPLIER = await masterChef.BONUS_MULTIPLIER();
+        let tx = await masterChef.connect(account).leaveStaking(amount);
+        let result = await tx.wait();
+        let blockBumber = ethers.utils.parseUnits(result.blockNumber.toString(), 0);
+        let pending = await pendingKiKi(pool, user, lpSupply, blockBumber, kikiPerBlock, totalAllocPoint, BONUS_MULTIPLIER);
+        expect(await kikiToken.balanceOf(account.address)).to.equal(oldBalanceInKiKi.add(pending).add(amount));
+        expect(await syrupBar.balanceOf(account.address)).to.equal(oldStakingInSyrupBar.sub(amount));
+        expect(await kikiToken.balanceOf(masterChef.address)).to.equal(oldStakingInMasterChef.sub(amount));
+    }
+
+    it("Deploy", async function () {
+        const mockKiKiToken = await ethers.getContractFactory('mockKiKiToken');
+        this.kikiToken = await mockKiKiToken.deploy("KiKiToken", "KIKI");
+        await this.kikiToken.deployed();
+        let SyrupBar = await ethers.getContractFactory('SyrupBar');
+        this.syrupBar = await SyrupBar.deploy(this.kikiToken.address);
+        await this.syrupBar.deployed();
+        let MasterChef = await ethers.getContractFactory('MasterChef');
+        this.perBlock = ethers.utils.parseUnits("10", 18);
+        let startBlock = 0;
+        this.masterChef = await MasterChef.deploy(this.kikiToken.address, this.syrupBar.address, this.perBlock, startBlock);
+        await this.masterChef.deployed();
+    })
+
+    it("mint test token", async function () {
+        const [, signer1, signer2] = await ethers.getSigners();
+        let amount = ethers.utils.parseUnits("10000000", 18);
+        let tx = await this.kikiToken.mint(signer1.address, amount);
+        await tx.wait();
+        tx = await this.kikiToken.mint(signer2.address, amount);
+        await tx.wait();
+        expect(await this.kikiToken.balanceOf(signer1.address)).to.equal(amount);
+        expect(await this.kikiToken.balanceOf(signer2.address)).to.equal(amount);
+        expect(await this.kikiToken.totalSupply()).to.equal(amount.mul(2));
+    })
+
+    it("transfer owner", async function () {
+        let tx = await this.kikiToken.transferOwnership(this.masterChef.address);
+        await tx.wait();
+        expect(await this.kikiToken.owner()).to.equal(this.masterChef.address);
+        tx = await this.syrupBar.transferOwnership(this.masterChef.address);
+        await tx.wait();
+        expect(await this.syrupBar.owner()).to.equal(this.masterChef.address);
+    })
+
+    it("aprove", async function () {
+        const [, signer1, signer2] = await ethers.getSigners();
+        let amount = await this.kikiToken.balanceOf(signer1.address);
+        let tx = await this.kikiToken.connect(signer1).approve(this.masterChef.address, amount);
+        await tx.wait();
+        expect(await this.kikiToken.allowance(signer1.address, this.masterChef.address)).to.equal(amount);
+        tx = await this.kikiToken.connect(signer2).approve(this.masterChef.address, amount);
+        await tx.wait();
+        expect(await this.kikiToken.allowance(signer2.address, this.masterChef.address)).to.equal(amount);
+    })
+
+    it("account1 enter staking", async function () {
+        const [, signer1] = await ethers.getSigners();
+        let amount = 0;
+        let tx = await this.masterChef.connect(signer1).enterStaking(amount);
+        await tx.wait();
+        amount = 1;
+        tx = await this.masterChef.connect(signer1).enterStaking(amount);
+        await tx.wait();
+        expect(await this.kikiToken.balanceOf(this.masterChef.address)).to.equal(amount);
+        expect(await this.syrupBar.balanceOf(signer1.address)).to.equal(amount);
+        let amount2 = ethers.utils.parseUnits("1000000", 18);
+        tx = await this.masterChef.connect(signer1).enterStaking(amount2);
+        await tx.wait();
+        expect(await this.kikiToken.balanceOf(this.masterChef.address)).to.equal(amount2.add(amount));
+        expect(await this.syrupBar.balanceOf(signer1.address)).to.equal(amount2.add(amount));
+    })
+
+    it("account1 harvest", async function () {
+        const [, signer1] = await ethers.getSigners();
+        let amount = ethers.utils.parseUnits('0', 18);
+        await leaveStaking(signer1, amount, this.kikiToken, this.syrupBar, this.masterChef);
+    })
+
+    it("account1 leave staking: 1 wei amount", async function () {
+        const [, signer1] = await ethers.getSigners();
+        let amount = ethers.utils.parseUnits("1", 0);
+        await leaveStaking(signer1, amount, this.kikiToken, this.syrupBar, this.masterChef);
+    })
+
+    it("account1 leave staking twice: 10 ether amount once", async function () {
+        const [, signer1] = await ethers.getSigners();
+        let amount = ethers.utils.parseUnits("10", 18);
+        let count = 2;
+        while(count > 0) {
+            await leaveStaking(signer1, amount, this.kikiToken, this.syrupBar, this.masterChef);
+            --count;
+        }
+    })
+
+    it("account2 enter staking", async function () {
+        const [, , signer2] = await ethers.getSigners();
+        let amount = ethers.utils.parseUnits("1000000", 18);
+        let oldStakingInMasterChef = await this.kikiToken.balanceOf(this.masterChef.address)
+        let tx = await this.masterChef.connect(signer2).enterStaking(amount);
+        await tx.wait();
+        expect(await this.kikiToken.balanceOf(this.masterChef.address)).to.equal(oldStakingInMasterChef.add(amount));
+        expect(await this.syrupBar.balanceOf(signer2.address)).to.equal(amount);
+    })
+
+    it("account2 harvest", async function () {
+        const [, , signer2] = await ethers.getSigners();
+        let amount = ethers.utils.parseUnits('0', 18);
+        await leaveStaking(signer2, amount, this.kikiToken, this.syrupBar, this.masterChef);
+    })
+
+    it("account2 leave staking", async function () {
+        const [, , signer2] = await ethers.getSigners();
+        let amount = ethers.utils.parseUnits("10", 18);
+        await leaveStaking(signer2, amount, this.kikiToken, this.syrupBar, this.masterChef);
+    })
+})

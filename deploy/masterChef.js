71449b687cdc18a4c77c0c8593991cf4cb7cd84c
王锦 <2535954590@qq.com>
2022-06-16 15:14:12 +0800

    deploy contracts on production

--- deploy/masterChef.js
+++ deploy/masterChef.js
@@ -28,7 +28,7 @@ module.exports = async function ({
   console.log("perBlock===> ", perBlock);
   await deploy('MasterChef', {
     from: deployer.address,
-    args: [KIKIToken, syrupBar.address, perBlock, 0],
+    args: [KIKIToken, syrupBar.address, perBlock, 14977655],
     log: true,
   });
 
@@ -36,6 +36,14 @@ module.exports = async function ({
   tx = await syrupBar.connect(deployer).transferOwnership(masterChef.address);
   tx = await tx.wait();
   console.log("transfer syrupBar contract owner to: ", masterChef.address);
+
+  let newMasterChefOwner = "0xab1dcF03ce47Cf5cc0cB1AdB06b772373d2A4E59";
+  if (hre.network.tags.staging) {
+    newMasterChefOwner = deployer.address;
+  }
+  tx = await masterChef.connect(deployer).transferOwnership(newMasterChefOwner);
+  tx = await tx.wait();
+  console.log("transfer masterChef contract owner to: ", newMasterChefOwner);
 
 };
 
